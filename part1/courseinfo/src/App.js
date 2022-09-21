aaa6306195f7b78b36cdfa0da63e94f3e13cdb7a
Patrick Christie <patrick.christie.dev@gmail.com>
2022-09-22 08:13:16 +0930

    Fixed and readded the content component after misintepreting the exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,6 +2,16 @@ const Header = (props) => {
   return <h1>{props.course}</h1>;
 };
 
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  );
+};
+
 const Part = (props) => {
   return (
     <>
@@ -18,27 +28,29 @@ const Total = (props) => {
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Part part={part1} />
-      <Part part={part2} />
-      <Part part={part3} />
+      <Content parts={parts} />
 
-      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+      <Total
+        total={parts[0].exercises + parts[1].exercises + parts[2].exercises}
+      />
     </div>
   );
 };
