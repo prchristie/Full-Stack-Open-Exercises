4a85870fd58e8445d4cf37da136a2911d83cc2fd
Patrick Christie <patrick.christie.dev@gmail.com>
2022-10-23 22:17:22 +1030

    added 3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,6 +29,24 @@ app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((p) => p.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((p) => p.id !== id);
+
+  response.status(204).end();
+});
+
 app.get("/info", (request, response) => {
   response.send(
     `<p>Phonebook has info for ${persons.length} people</p>
