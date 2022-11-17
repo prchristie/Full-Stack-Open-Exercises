bf7f07fab469df07b7970d25507a955e02d68612
Patrick Christie <patrick.christie.dev@gmail.com>
2022-11-17 16:27:47 +1030

    Added controller and loggers

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const config = require("./utils/config");
 const app = express();
 const mongoose = require("mongoose");
+const blogsRouter = require("./controllers/blog");
 
 const cors = require("cors");
 
@@ -9,5 +10,7 @@ mongoose.connect(config.MONGODB_URI);
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/blogs", blogsRouter);
 
 module.exports = app;
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,22 +1,8 @@
 require("dotenv").config();
 const app = require("./app");
-const Blog = require("./models/blog");
 const config = require("./utils/config");
-
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+const logger = require("./utils/logger");
 
 app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+  logger.info(`Server running on port ${config.PORT}`);
 });
