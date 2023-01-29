86fa425116ed87658b2cb25761415db59202d43c
Patrick Christie <patrick.christie.dev@gmail.com>
2023-01-29 20:42:19 +1030

    Added 6.3

---
+++ part6/redux-anecdotes/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(reducer)
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+
+store.subscribe(() => root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+))
